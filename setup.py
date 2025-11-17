9057b7ca8e4ec5b058fec4c733a4cedf78479467
opfernandez <opfernandez@uma.es>
2025-11-17 13:17:15 +0100

    fix .env dependence

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('lib/' + package_name, [package_name + '/langgraph_home_assistant.py']),
-        ('share/' + package_name, ['.env']),
+        ('share/' + package_name, ['*.env']),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'),
             glob(os.path.join('launch', '*.launch.py'))),
